fa56ad58c3f6ba008ed34fb0ee97228ba95fd5d1
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2022-09-23 08:29:08 -0600

    Add project files.

---
+++ Vehicle.UnitTests/VehicleTests.cs
@@ -0,0 +1,28 @@
+namespace Vehicle.UnitTests
+{
+    public class VehicleTests
+    {
+        [Fact]
+        public void WhenCreated_HasCorrectProperties()
+        {
+            Vehicle megansCar = new Vehicle(4, "Green");
+
+            Assert.Equal(4, megansCar.NumberOfWheels);
+            Assert.Equal("Green", megansCar.Color);
+            Assert.Equal(0, megansCar.MilesDriven);
+        }
+
+        // This test needs some work!
+        [Fact]
+        public void Summary_ReturnsInfoAboutVehicle()
+        {
+            Vehicle andysBike = new Vehicle(2, "Blue");
+
+            var expected = "This Blue vehicle has 2 wheels and has driven 0 miles";
+
+
+        }
+
+        // Add more tests here!
+    }
+}
---
+++ Vehicle/Vehicle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    public class Vehicle
+    {
+        public int NumberOfWheels { get; }
+        public string Color { get; private set;  }
+        public int MilesDriven { get; private set; }
+
+        public Vehicle(int numberOfWheels, string color)
+        {
+            NumberOfWheels = numberOfWheels;
+            Color = color;
+            MilesDriven = 0;
+        }
+
+        public string Sumary()
+        {
+            string summary = $"This {Color} vehicle has {NumberOfWheels} wheels, and has driven {MilesDriven} miles.";
+            return summary;
+        }
+
+        public void Drive()
+        {
+            MilesDriven += 5;
+        }
+
+        public void Paint(string newColor)
+        {
+            Color = newColor;
+        }
+    }
+}
